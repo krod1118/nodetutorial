19bf9b2223d06c13e5976bbfe6f5a5acbdfb0bfb
krod1118 <130703823+krod1118@users.noreply.github.com>
2024-09-26 08:16:55 -0500

    type corrections

--- node_react/client/src/App.js
+++ node_react/client/src/App.js
@@ -1,13 +1,24 @@
+import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-       
-       <p>
-        Did i do this properly?
-       </p>
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          And I only cried for 3 hours making this       </p>
+        <p>
+          so I'm a little proud of myself
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
       </header>
     </div>
   );
